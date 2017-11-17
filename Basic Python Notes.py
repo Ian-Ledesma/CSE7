81107961714e2d691b76607e7af8fc527d660c12
Mr. Wiebe <kyle.wiebe@fresnounified.org>
2017-11-17 15:25:58 -0800

    Initial commit

---
+++ Basic Python Notes.py
@@ -0,0 +1,21 @@
+print('Hello World')
+print("Welcome to Class!")
+
+# This is how you add a comment
+# They are really useful!
+# The program ignores these lines
+
+'''This is a bunch of text
+that will all be ignored
+because it is in between 3 quotation
+marks
+'''
+
+# Variables
+a = "Hello world"
+print(a)
+
+b = 5
+c = 7
+print(b + c)
+print(-1 - (-5))
