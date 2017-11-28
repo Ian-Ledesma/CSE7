da18eb626fa36f98fd1b35aa8aa2f6a1b00529b4
Mr. Wiebe <kyle.wiebe@fresnounified.org>
2017-11-28 10:46:06 -0800

    Added previous notes
    11.27.17

---
+++ Notes.py
@@ -0,0 +1,32 @@
+print("Hello World!")
+
+# Mr. Wiebe (Use ctrl+/ to toggle comments)
+
+print(3 + 5)
+print(5 - 3)
+print(5 * 3)
+print(6 / 2)
+print(3 ** 2)
+
+print()  # Creates a blank line
+print("See if you can figure this out")
+print(15 % 5)
+
+# Variables
+car_name = "Wiebe Mobile"
+car_type = "Lamborghini Sesto Elemento"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline Printing
+print("My car is the %s." % car_name)
+print("My car is the %s. It is a %s" % (car_name, car_type))
+
+# Taking input
+name = input("What is your name? ")
+print("Hello %s." % name)
+# print(name)
+
+age = input("How old are you? ")
+print("%s?! That old? You belong in a museum." % age)
+print("That or a retirement home.")
